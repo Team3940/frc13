41111298df1989ed0e1853c490e3811b6f24a124
Greg McCoy <Greg.R.McCoy@gmail.com>
2013-01-21 19:34:33 -0500

    Experimenting with vision filters

--- vision/GoalFinder.cpp
+++ vision/GoalFinder.cpp
@@ -60,16 +60,9 @@ void GoalFinder::DoVision()
 
 JpegFromImaq * GoalFinder::GetDebugImage()
 {
-  Point point1;
-  point1.x = 100;
-  point1.y = 100;
-
-  Point point2;
-  point2.x = 200;
-  point2.y = 200;
-
   // Image *overlay = imaqCreateImage(IMAQ_IMAGE_RGB, DEFAULT_BORDER_SIZE);
-  ColorImage overlay(IMAQ_IMAGE_RGB);
+  BinaryImage overlay;
+  BinaryImage overlayResult;
 
   if (!camera->GetImage(&img)) {
     std::cout << "Image not acquired :(" << std::endl;
@@ -78,21 +71,60 @@ JpegFromImaq * GoalFinder::GetDebugImage()
 
   std::cout << "Got image" << std::endl;
 
+
+
   BinaryImage result;
+  imaqColorThreshold(overlay.GetImaqImage(), img.GetImaqImage(), 255, IMAQ_RGB, &redRange, &greenRange, &blueRange);
   imaqColorThreshold(result.GetImaqImage(), img.GetImaqImage(), 1, IMAQ_RGB, &redRange, &greenRange, &blueRange);
   imaqConvexHull(result.GetImaqImage(), result.GetImaqImage(), 8);
   std::cout << "convex hull error:  " << imaqGetLastErrorFunc() << std::endl;
-  //imaqOverlayLine(result.GetImaqImage(), point1, point2, &color, "");
-  //imaqOverlayText(result.GetImaqImage(), point1, "Primary Goal", &color, &options, "");
-  //imaqMergeOverlay(overlay.GetImaqImage(), result.GetImaqImage(), NULL, 256, "");
+
+  // filter
+  int pParameter[1] = {35};
+  double pLower[1] = {10000};
+  double pUpper[1] = {35000};
+  int pCalibrated[1] = {0};
+  int pExclude[1] = {0};
+
+  ParticleFilterCriteria2* pParticleCriteria = NULL;
+  ParticleFilterOptions pParticleFilterOptions;
+  int pNumParticles;
+
+  pParticleCriteria = (ParticleFilterCriteria2*)malloc(sizeof(ParticleFilterCriteria2));
+  pParticleCriteria[0].parameter = (MeasurementType)pParameter[0];
+  pParticleCriteria[0].lower = pLower[0];
+  pParticleCriteria[0].upper = pUpper[0];
+  pParticleCriteria[0].calibrated = pCalibrated[0];
+  pParticleCriteria[0].exclude = pExclude[0];
+
+  pParticleFilterOptions.rejectMatches = FALSE;
+  pParticleFilterOptions.rejectBorder = 0;
+  pParticleFilterOptions.connectivity8 = TRUE;
+  imaqParticleFilter3(result.GetImaqImage(), result.GetImaqImage(), pParticleCriteria, 1, &pParticleFilterOptions, NULL, &pNumParticles);
+  free(pParticleCriteria);
+  std::cout << "Filtered particles:  " << pNumParticles << std::endl;
+  // end filter
 
   // print out the center mass for each particle to the console
   int particleCount = result.GetNumberParticles();
   for (int particle = 0; particle < particleCount; particle++) {
     ParticleAnalysisReport report = result.GetParticleAnalysisReport(particle);
-    std::cout << "Particle " << particle << " X:  " << report.center_mass_x_normalized << "  Y:  " << report.center_mass_y_normalized << std::endl; 
+    std::cout << "Particle " << particle << " X:  " << report.center_mass_x_normalized << "  Y:  " << report.center_mass_y_normalized << std::endl;
+
+    // draw line at center mass
+    Point point1;
+    point1.x = (int) ((report.center_mass_x_normalized + 1.0) * 320.0);
+    point1.y = (int) ((report.center_mass_y_normalized + 1.0) * 240.0);
+
+    Point point2;
+    point2.x = point1.x + 10;
+    point2.y = point1.y + 10;
+    imaqOverlayLine(overlay.GetImaqImage(), point1, point2, &color, "");
   }
 
-  JpegFromImaq *jpeg = new JpegFromImaq(overlay.GetImaqImage());
+  //imaqOverlayText(overlay.GetImaqImage(), point1, "Primary Goal", &color, &options, "");
+  imaqMergeOverlay(overlayResult.GetImaqImage(), overlay.GetImaqImage(), NULL, 256, "");
+
+  JpegFromImaq *jpeg = new JpegFromImaq(overlayResult.GetImaqImage());
   return jpeg;
 }
